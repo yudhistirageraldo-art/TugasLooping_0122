5eea40a4c1e5862a685adc41bee7a3ed7e852c3c
yudhistirageraldo <yudhistirageraldo@gmail.com>
2026-04-01 15:23:21 +0700

    "adalah bilangan prima"

--- file1.cpp
+++ file1.cpp
@@ -27,4 +27,22 @@ bool isPrima(int x){
 }
 
 // Function cek bilangan fibonacci
-bool isFIBONACCI(int x) {}
+bool isFIBONACCI(int x) {
+    int a = 0, b = 1, c;
+
+     while (a <= x) {
+        if (a == x) {
+            return true;
+        }
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return false;
+}
+
+// Prosedur tampil hasil
+void tampilHasilPrima() {
+    if (isPrima(n)) {
+             cout << n << " adalah bilangan prima." << endl;
+    } else {
